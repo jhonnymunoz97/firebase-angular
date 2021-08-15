cc141dfc8341f818ca10dc7f5afb56904d9dda8a
jhonnymunoz97 <jhonnyjmc97@gmail.com>
2021-08-14 21:00:10 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirebaseComponent } from './pages/firebase/firebase.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'firebase',
+    component: FirebaseComponent
+  },
+  {
+    path:'**',
+    pathMatch:'full',
+    redirectTo:'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FirebaseComponent } from './pages/firebase/firebase.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FirebaseComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
